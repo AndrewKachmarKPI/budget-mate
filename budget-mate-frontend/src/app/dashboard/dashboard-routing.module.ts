20cde7a9b782e903f415d33c62698a3b48f7586d
Andrea04172004 <kachmar24@ukr.net>
2023-04-21 13:20:05 +0300

    integrate template

--- budget-mate-frontend/src/app/dashboard/dashboard-routing.module.ts
+++ budget-mate-frontend/src/app/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,19 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {BudgetDashboardComponent} from "./budget-dashboard/budget-dashboard.component";
 
 const routes: Routes = [
-  {path: '', component: DashboardComponent}
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'budget',
+        component: BudgetDashboardComponent,
+      }
+    ]
+  }
 ];
 
 @NgModule({
--- budget-mate-frontend/src/app/dashboard/dashboard.module.ts
+++ budget-mate-frontend/src/app/dashboard/dashboard.module.ts
@@ -3,11 +3,12 @@ import {CommonModule} from '@angular/common';
 
 import {DashboardRoutingModule} from './dashboard-routing.module';
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import { BudgetDashboardComponent } from './budget-dashboard/budget-dashboard.component';
 
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, BudgetDashboardComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule
