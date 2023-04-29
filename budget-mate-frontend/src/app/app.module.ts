ff9d00ad8bbf442d9774a682cc6690a7b3741e6c
KhilchukArtemIP-14 <89968135+KhilchukArtemIP-14@users.noreply.github.com>
2023-04-29 15:25:39 +0300

    Initial commit
    Raw login page with some extra stuff from front-end authorization functionality

--- budget-mate-frontend/src/app/app.module.ts
+++ budget-mate-frontend/src/app/app.module.ts
@@ -6,10 +6,27 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {DashboardModule} from "./dashboard/dashboard.module";
 import {AppRoutingModule} from "./app-routing.module";
 import {ToastrModule} from "ngx-toastr";
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import {HttpClientModule} from "@angular/common/http";
+import { httpInterceptorProviders } from './_helpers/http.interceptor';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    BoardAdminComponent,
+    BoardModeratorComponent,
+    BoardUserComponent,
   ],
   imports: [
     BrowserModule,
@@ -18,8 +35,10 @@ import {ToastrModule} from "ngx-toastr";
     DashboardModule,
     AppRoutingModule,
     ToastrModule.forRoot(),
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [httpInterceptorProviders,RegisterComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {
