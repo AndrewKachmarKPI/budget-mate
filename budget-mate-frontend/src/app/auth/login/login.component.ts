0bf67e8dcb19321d4d5a2ce1d8838196519b5a74
Andrea04172004 <kachmar24@ukr.net>
2023-05-07 14:43:08 +0300

    changes

--- budget-mate-frontend/src/app/auth/login/login.component.ts
+++ budget-mate-frontend/src/app/auth/login/login.component.ts
@@ -4,18 +4,32 @@ import {StorageService} from '../../_services/storage.service';
 import {Router} from "@angular/router";
 import {ToastrService} from "ngx-toastr";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  animations: [trigger('load', [
+    transition(':enter', [
+      style({opacity: 0}),
+      animate(600, style({opacity: 1}))
+    ]),
+    transition(':leave', [
+      style({opacity: 1}),
+      animate(600, style({opacity: 0}))
+    ])
+
+  ])
+  ]
 })
 export class LoginComponent implements OnInit {
   public passwordType: 'text' | 'password' = 'password';
   public formGroup = new FormGroup({
-    username: new FormControl('', Validators.compose([Validators.required,Validators.minLength(3)])),
+    username: new FormControl('', Validators.compose([Validators.required, Validators.minLength(3)])),
     password: new FormControl('', Validators.compose([Validators.required, Validators.minLength(6)])),
   });
+  public isSubmitted: boolean = false;
 
   constructor(private authService: AuthService,
               private router: Router, private toaster: ToastrService,
@@ -35,14 +49,14 @@ export class LoginComponent implements OnInit {
 
 
   onSubmit(): void {
-    console.log("FORM", this.controls);
+    this.isSubmitted = true;
     if (this.formGroup.valid) {
       const username: string = this.controls.username.value;
       const password: string = this.controls.password.value;
 
       this.authService.login(username, password).subscribe({
         next: data => {
-          this.router.navigate(['/dashboard']).then(()=>{
+          this.router.navigate(['/dashboard']).then(() => {
             this.toaster.success("Successfully logged in!");
           });
         },
--- budget-mate-frontend/src/app/auth/register/register.component.ts
+++ budget-mate-frontend/src/app/auth/register/register.component.ts
@@ -1,30 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../_services/auth.service';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../_services/auth.service';
 import {ToastrService} from "ngx-toastr";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {StorageService} from '../../_services/storage.service';
 import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {Router} from "@angular/router";
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-register',
   templateUrl: 'register.component.html',
-  styleUrls: ['register.component.css']
+  styleUrls: ['register.component.css'],
+  animations: [trigger('load', [
+    transition(':enter', [
+      style({opacity: 0}),
+      animate(600, style({opacity: 1}))
+    ]),
+    transition(':leave', [
+      style({opacity: 1}),
+      animate(600, style({opacity: 0}))
+    ])
+
+  ])
+  ]
 })
 export class RegisterComponent implements OnInit {
 
   public constructor(private authService: AuthService,
-              private router: Router, private toaster: ToastrService,
-              private storageService: StorageService) {
+                     private router: Router, private toaster: ToastrService,
+                     private storageService: StorageService) {
   }
 
-  public passwordType:'password'|'text'='password';
-  public formGroup= new FormGroup({
+  public passwordType: 'password' | 'text' = 'password';
+  public isSubmitted: boolean = false;
+  public formGroup = new FormGroup({
     username: new FormControl('', Validators.compose([Validators.required, Validators.minLength(3)])),
     email: new FormControl('', Validators.compose([Validators.required, Validators.email])),
     password: new FormControl('', Validators.compose([Validators.required, Validators.minLength(6)])),
   });
+
   changePasswordVisibility() {
     this.passwordType = this.passwordType == 'password' ? 'text' : 'password';
   }
@@ -34,13 +49,14 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(): void {
-    if(this.formGroup.valid){
+    this.isSubmitted = true;
+    if (this.formGroup.valid) {
       const username: string = this.formGroup.controls.username.value;
-      const email= this.formGroup.controls.email.value;
+      const email = this.formGroup.controls.email.value;
       const password: string = this.formGroup.controls.password.value;
       this.authService.register(username, email, password).subscribe({
         next: data => {
-          this.router.navigate(['/auth/login']).then(()=>{
+          this.router.navigate(['/auth/login']).then(() => {
             console.log(data);
             this.toaster.success("Successfully signed up!");
           });
