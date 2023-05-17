ea77dc94db0332047a00378082ff19c00a33f833
Khilchuk-Artem <artem.khilchuk@gmail.com>
2023-05-17 16:46:31 +0300

    Added faq and minor changes
    Changed billing details and added kostil to left navbar scrolling
    
    Also done some bs with dependencies and now it throws some shit though still compiles

--- budget-mate-frontend/src/app/dashboard/billing-details/billing-details.component.ts
+++ budget-mate-frontend/src/app/dashboard/billing-details/billing-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import { ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-billing-details',
@@ -8,27 +10,70 @@ import { Component, OnInit } from '@angular/core';
 export class BillingDetailsComponent implements OnInit {
 
   invoices: any = [
-    { code:'1XBDAS1',amount: 2000,plan: 'Plan1',invoiceDate:'11.05.2023',datePaid:'12.05.23',status:'Active',paymentMethod:'karta1',invoiceNumber:1234},
-    { code:'1XBDAS2',amount: 2000,plan: 'Plan2',invoiceDate:'11.05.2023',datePaid:'12.05.23',status:'Completed',paymentMethod:'karta2',invoiceNumber:1234},
-    { code:'1XBDAS3',amount: 2000,plan: 'Plan3',invoiceDate:'11.05.2023',datePaid:'12.05.23',status:'Scheduled',paymentMethod:'karta3',invoiceNumber:1234},
-    { code:'1XBDAS4',amount: 2000,plan: 'Plan4',invoiceDate:'11.05.2023',datePaid:'12.05.23',status:'Pending',paymentMethod:'karta4',invoiceNumber:1234},
+    { code:'1XBDAS1',amount: 9,plan: 'Plan1',invoiceDate:'11.05.2023',datePaid:'12.05.23',status:'Active',paymentMethod:'karta1',invoiceNumber:1234},
+    { code:'1XBDAS2',amount: 169,plan: 'Plan2',invoiceDate:'11.05.2023',datePaid:'12.05.23',status:'Completed',paymentMethod:'karta2',invoiceNumber:1234},
+    { code:'1XBDAS3',amount: 169,plan: 'Plan3',invoiceDate:'11.05.2023',datePaid:'12.05.23',status:'Scheduled',paymentMethod:'karta3',invoiceNumber:1234},
+    { code:'1XBDAS4',amount: 99,plan: 'Plan4',invoiceDate:'11.05.2023',datePaid:'12.05.23',status:'Pending',paymentMethod:'karta4',invoiceNumber:1234},
   ];
 
-  // Create an empty dictionary
+  cards:any=[
+    {type:"mastercard",ownerName:"Tom McBride", expiration:"12/26", number:"4716313599689856",cvv:"133",isPrimary:true},
+    {type:"visa",ownerName:"Mildred Melnyk", expiration:"12/26", number:"5525132581415896",cvv:"144",isPrimary:false},
+  ]
+
    public statusesAndClasses = new Map<string, String>([
     ["Completed", 'badge bg-label-success me-1'],
      ["Active",'badge bg-label-primary me-1'],
      ["Scheduled", 'badge bg-label-info me-1'],
      ["Pending", 'badge bg-label-warning me-1']
    ]);
-
-  constructor() { }
+  public cardFormGroup = new FormGroup({
+    ownerName: new FormControl('', Validators.compose([Validators.required])),
+    number: new FormControl('', Validators.compose([Validators.required])),
+    expiration: new FormControl('', Validators.compose([Validators.required])),
+    cvv: new FormControl('', Validators.compose([Validators.required])),
+  });
+  constructor(private elementRef: ElementRef) { }
 
 
   ngOnInit(): void {
-    this.invoices.forEach((_invoice) => {
-      _invoice.isExpanded = true;
-    });
   }
+  saveCard():void{
+    console.log("dodav kartu")
+    console.log(this.cardFormGroup)
+    var tempData={type:"",ownerName:"",number:"",expiration:"",cvv:"",isPrimary:false};
+    tempData.type=this.determineCardType(this.cardFormGroup.value.number)
+    tempData.ownerName=this.cardFormGroup.value.ownerName
+    tempData.number=this.cardFormGroup.value.number
+    tempData.expiration=this.cardFormGroup.value.expiration
+    tempData.cvv=this.cardFormGroup.value.cvv
+    //treba she podumati yak handleti primarity of cards
+    //tempData.isPrimary=false
+    this.cardFormGroup.reset()
+    //tipa she v bd shletsia
+    this.cards.push(tempData)
+  }
+   determineCardType(cardNumber: string): string {
+    // Remove any non-digit characters from the card number
+    const cleanedNumber = cardNumber.replace(/\D/g, '');
 
+    // Define regular expressions for different card types
+    const cardPatterns = {
+      visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
+      mastercard: /^5[1-5][0-9]{14}$/,
+    };
+
+    // Check the card number against the defined patterns
+    for (const cardType in cardPatterns) {
+      if (cardPatterns[cardType].test(cleanedNumber)) {
+        console.log(cardType)
+        return cardType;
+      }
+    }
+    return 'Unknown';
+  }
+  closeCardModal(){
+    this.cardFormGroup.reset()
+  }
 }
+
---
+++ budget-mate-frontend/src/app/dashboard/frequenty-asked-questions/frequenty-asked-questions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frequenty-asked-questions',
+  templateUrl: './frequenty-asked-questions.component.html',
+  styleUrls: ['./frequenty-asked-questions.component.css']
+})
+export class FrequentyAskedQuestionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
