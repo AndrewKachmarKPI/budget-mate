2425e7d853be230c195be9e5f8c024ca344bf133
Andrea04172004 <kachmar24@ukr.net>
2023-05-20 18:08:16 +0300

    a lot o changes

---
+++ budget-mate-frontend/src/app/auth/models/role-dto.ts
@@ -0,0 +1,7 @@
+export class RoleDto {
+  constructor(public icon?: string,
+              public style?: string,
+              public roleName?: string,
+              public created?: string) {
+  }
+}
