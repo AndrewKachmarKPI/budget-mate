1e3a80e3c8b968a6e17e74fc2a97e8823633915f
Andrea04172004 <kachmar24@ukr.net>
2023-05-21 11:31:08 +0300

    create budget backend

--- budget-mate-frontend/src/app/dashboard/budget-view/budget-view.component.ts
+++ budget-mate-frontend/src/app/dashboard/budget-view/budget-view.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
+import {Component, OnInit, Input, OnChanges, SimpleChanges, ViewChild} from '@angular/core';
 import {formatDate} from '@angular/common';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import Cleave from "cleave.js";
-import { ChartComponent } from "ng-apexcharts";
+import {ChartComponent} from "ng-apexcharts";
 import {
   ApexAxisChartSeries,
   ApexChart,
@@ -34,21 +34,47 @@ export type PieChartOptions = {
   responsive: ApexResponsive[];
   labels: any;
 };
+
 @Component({
   selector: 'app-budget-view',
   templateUrl: './budget-view.component.html',
   styleUrls: ['./budget-view.component.css']
 })
 export class BudgetViewComponent implements OnInit, OnChanges {
-  public categories=[{icon:"bx bx-pie-chart-alt bx-xs",name:"Entertainment"},
-                                              {icon:"bx bx-heart bx-xs",name:"Gachi"}]
-  private expenses=[
-    {name:"Kek1",amount:500,date:"2023-05-12",category: {icon:"bx bx-pie-chart-alt bx-xs",name:"Entertainment"}},
-    {name:"Kek2",amount:300,date:"2023-05-11",category: {icon:"bx bx-heart bx-xs",name:"Gachi"}},
-    {name:"Kek3",amount:200,date:"2023-05-10",category: {icon:"bx bx-pie-chart-alt bx-xs",name:"Entertainment"}},
-    {name:"Kek4",amount:200,date:"2023-05-09",category: {icon:"bx bx-pie-chart-alt bx-xs",name:"Entertainment"}},
-    {name:"Kek5",amount:200,date:"2023-05-08",category: {icon:"bx bx-pie-chart-alt bx-xs",name:"Entertainment"}},
-    {name:"Kek6",amount:200,date:"2023-05-07",category: {icon:"bx bx-pie-chart-alt bx-xs",name:"Entertainment"}},
+  public categories = [{icon: "bx bx-pie-chart-alt bx-xs", name: "Entertainment"},
+    {icon: "bx bx-heart bx-xs", name: "Gachi"}]
+  private expenses = [
+    {
+      name: "Kek1",
+      amount: 500,
+      date: "2023-05-12",
+      category: {icon: "bx bx-pie-chart-alt bx-xs", name: "Entertainment"}
+    },
+    {name: "Kek2", amount: 300, date: "2023-05-11", category: {icon: "bx bx-heart bx-xs", name: "Gachi"}},
+    {
+      name: "Kek3",
+      amount: 200,
+      date: "2023-05-10",
+      category: {icon: "bx bx-pie-chart-alt bx-xs", name: "Entertainment"}
+    },
+    {
+      name: "Kek4",
+      amount: 200,
+      date: "2023-05-09",
+      category: {icon: "bx bx-pie-chart-alt bx-xs", name: "Entertainment"}
+    },
+    {
+      name: "Kek5",
+      amount: 200,
+      date: "2023-05-08",
+      category: {icon: "bx bx-pie-chart-alt bx-xs", name: "Entertainment"}
+    },
+    {
+      name: "Kek6",
+      amount: 200,
+      date: "2023-05-07",
+      category: {icon: "bx bx-pie-chart-alt bx-xs", name: "Entertainment"}
+    },
   ]
   public expenseFormGroup = new FormGroup({
     name: new FormControl('', Validators.compose([Validators.required])),
@@ -62,10 +88,10 @@ export class BudgetViewComponent implements OnInit, OnChanges {
   });
 
   @Input()
-  public selectedCategory="None";
-  public expensesView=this.expenses;
-  public budget=5000;
-  public totalExpenses=this.expenses.reduce((accumulator, currentItem) => {
+  public selectedCategory = "None";
+  public expensesView = this.expenses;
+  public budget = 5000;
+  public totalExpenses = this.expenses.reduce((accumulator, currentItem) => {
     if (currentItem.amount > 5) {
       return accumulator + currentItem.amount;
     }
@@ -78,54 +104,56 @@ export class BudgetViewComponent implements OnInit, OnChanges {
   public barChartOptions: Partial<BarChartOptions>;
 
   constructor() {
-    var categoriesAndSums=this.expenses.reduce((result, item) => {
-      const { category, amount } = item;
+    var categoriesAndSums = this.expenses.reduce((result, item) => {
+      const {category, amount} = item;
       if (!result[category.name]) {
-        result[category.name] = { category, sum: 0 };
+        result[category.name] = {category, sum: 0};
       }
       result[category.name].sum += amount;
       return result;
     }, {});
-  this.pieChartOptions= {
-    series: Object.values(categoriesAndSums).map(({ sum }) => sum),
-    chart: {
-      width: 480,
-      type: "pie",
-    },
-    labels:  Object.values(categoriesAndSums).map(({ category }) => category.name),
-    responsive: [
-      {
-        breakpoint: 480,
-        options: {
-          chart: {
-            width: 200
-          },
-          legend: {
-            position: "bottom"
+    this.pieChartOptions = {
+      series: Object.values(categoriesAndSums).map(({sum}) => sum),
+      chart: {
+        width: 480,
+        type: "pie",
+      },
+      labels: Object.values(categoriesAndSums).map(({category}) => category.name),
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: "bottom"
+            }
           }
         }
-      }
-    ],
-  };
+      ],
+    };
+
+
     const categoriesAndSumsTwo: { [key: string]: { category: Category; sum: number } } = this.expenses.reduce((result, item) => {
-      const { category, amount } = item;
+      const {category, amount} = item;
       if (!result[category.name]) {
-        result[category.name] = { category, sum: 0 };
+        result[category.name] = {category, sum: 0};
       }
       result[category.name].sum += amount;
       return result;
     }, {});
 
-    var topFiveCategories=Object.values(categoriesAndSumsTwo)
+    var topFiveCategories = Object.values(categoriesAndSumsTwo)
       .sort((a, b) => b.sum - a.sum)
       .slice(0, 5)
-      .map(({ category, sum }) => ({ category:category, sum }));
-    console.log(Object.values(topFiveCategories).map(({ category }) => category.name))
-    this.barChartOptions= {
-      series: [{ data: Object.values(topFiveCategories).map(({ sum }) => sum) }],
+      .map(({category, sum}) => ({category: category, sum}));
+    console.log(Object.values(topFiveCategories).map(({category}) => category.name))
+    this.barChartOptions = {
+      series: [{data: Object.values(topFiveCategories).map(({sum}) => sum)}],
       chart: {
         type: "bar",
-        height: 350
+        height: 350,
       },
       plotOptions: {
         bar: {
@@ -137,7 +165,7 @@ export class BudgetViewComponent implements OnInit, OnChanges {
       },
       xaxis: {
         categories: Object.values(topFiveCategories)
-          .map(({ category }) => category.name)
+          .map(({category}) => category.name)
       }
     };
   }
@@ -151,33 +179,38 @@ export class BudgetViewComponent implements OnInit, OnChanges {
       });
     }
   }
+
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['selectedCategory']) {
       this.categoryFilterSelected(this.selectedCategory);
     }
   }
-  categoryFilterSelected(categoryName:string){
-    if(categoryName==="None"){
-      this.expensesView=this.expenses;
+
+  categoryFilterSelected(categoryName: string) {
+    if (categoryName === "None") {
+      this.expensesView = this.expenses;
       return
     }
-    this.expensesView=this.expenses.filter(item=>item.category.name===categoryName);
+    this.expensesView = this.expenses.filter(item => item.category.name === categoryName);
   }
 
-  saveExpense(){
-     var tempExpense=    {
-       name:this.expenseFormGroup.value.name,
-       amount:parseInt(this.expenseFormGroup.value.amount),
-       date:this.expenseFormGroup.value.date,
-       category:this.categories.find(item => item.name === this.expenseFormGroup.value.categoryName)}
-     this.expenses.push(tempExpense)
-      console.log(this.expenses)
-     this.clearExpenseForm()
+  saveExpense() {
+    var tempExpense = {
+      name: this.expenseFormGroup.value.name,
+      amount: parseInt(this.expenseFormGroup.value.amount),
+      date: this.expenseFormGroup.value.date,
+      category: this.categories.find(item => item.name === this.expenseFormGroup.value.categoryName)
+    }
+    this.expenses.push(tempExpense)
+    console.log(this.expenses)
+    this.clearExpenseForm()
   }
-  clearExpenseForm(){
+
+  clearExpenseForm() {
     this.expenseFormGroup.reset()
   }
-  clearCategoryForm(){
+
+  clearCategoryForm() {
     this.categoryFormGroup.reset()
   }
 }
